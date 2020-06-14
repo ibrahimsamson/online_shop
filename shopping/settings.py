968e1ee9e27965367ee6475641559ae6efef21f9
ibrahimsamson <toddler1998@yahoo.com>
2020-06-14 13:14:58 +0800

    added list and details templates and setup static settings

--- shopping/settings.py
+++ shopping/settings.py
@@ -16,6 +16,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+# images
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
