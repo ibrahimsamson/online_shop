8c3b704c0d6efcc53dc2109ab03f7568a185b6b8
ibrahimsamson <toddler1998@yahoo.com>
2020-06-21 01:48:13 +0800

    add some static files

--- shopping/settings/base.py
+++ shopping/settings/base.py
@@ -5,6 +5,7 @@ from decouple import config
 env = environ.Env(
     DEBUG=(bool, False)
 )
+env.read_env(str("settings.env"))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(
